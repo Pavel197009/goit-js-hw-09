be7528b542714621471393daec1bbaaf7501714d
Pavel197009 <parutman@gmail.com>
2025-07-05 14:14:02 +0300

    Add files via upload

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,44 +1,43 @@
 function handleSubmit(event) {
   event.preventDefault();
-  const form = event.target;
+  const form = event.currentTarget;
   const login = form.elements.email.value;
   const message = form.elements.message.value;
   
-  if (login === "" || message === "") {
-    return alert("All form fields must be filled in");
+  if (login.trim() === "" || message.trim() === "") {             // проверка обрезанных input-ов на пустое значение с возвратом алерта 
+    return alert("Fill please all fields");
   }
 
-  console.log({email:login, message:message});
-  form.reset();
+  console.log({email:login, message:message});                    // выдача в консоли значений объекта
+  form.reset();                                                   // очистка формы после submit
+  localStorage.removeItem(LOCAL_KEY);                             // удаление ключа локального хранилища
 }
 
 function handleInputData(event) {
     const form = event.target;
-    formData = { email: email.value, message: message.value };
-    localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+    formData[`"${event.target.name}"`] = event.target.value;      // замена соответствующего поля объекта по таргету
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));    // запись объекта в локальное хранилище
   }
 
-function reloadData() {
-    if (formData) {
-      email.value = formData.email || '';
-      message.value = formData.message || '';
-    }
+function reloadData() {                                                   // первоначальная инициализация полей формы
+  formData = JSON.parse(localStorage.getItem("email","message")) || {};   // чтение данных с LocalStorage
+  email.value = formData.email || '';                                     // запись данных в поля Input
+  message.value = formData.message || '';
 }
   
-const LOCAL_KEY = 'feedback-form-state';
+const LOCAL_KEY = 'feedback-form-state';                          // инициализация ключа
 
-let formData = {
+let formData = {                                                  // инициализация объекта для работы с LocalStorage
     email: "",
     message: "",
 }
 
-const feedbackForm = document.querySelector(".feedback-form");
+const feedbackForm = document.querySelector(".feedback-form");    // инициализация объекта формы
 
-feedbackForm.addEventListener("submit", handleSubmit);
-feedbackForm.addEventListener('input', handleInputData);
+feedbackForm.addEventListener("submit", handleSubmit);            // добавление обработчика submit
+feedbackForm.addEventListener('input', handleInputData);          // добавление обработчика input
 
-formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
-const { email, message } = feedbackForm.elements;
+const { email, message } = feedbackForm.elements;                 // деструктуризируем элементы формы
 reloadData();
 
 
